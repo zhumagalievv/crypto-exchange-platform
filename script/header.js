43a8a764f8186001cfcfec6bc48692eae7c143b2
zhumagalievv <zhumagalievkanagat@gmail.com>
2021-11-09 21:36:10 +0600

    header

---
+++ script/header.js
@@ -0,0 +1,6 @@
+document.addEventListener('click',function(e){
+    // Hamburger menu
+    if(e.target.classList.contains('hamburger-toggle')){
+      e.target.children[0].classList.toggle('active');
+    }
+  }) 
